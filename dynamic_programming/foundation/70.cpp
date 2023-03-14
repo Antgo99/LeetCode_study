9ef84ee7b8308fcfffc76a1e986b5914f8c5c939
Antgo99 <52596133+Antgo99@users.noreply.github.com>
2023-03-14 22:12:49 +0800

    Create 70.cpp

---
+++ dynamic_programming/foundation/70.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        vector<int> dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            //dp[i] = dp[i-1] + dp[i-2];
+            int tmp=dp[1]+dp[2];
+            dp[1] = dp[2];
+            dp[2] = tmp;
+        }
+        return dp[2];
+    }
+};
