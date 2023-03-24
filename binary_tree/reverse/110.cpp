4b9d49f85a287549c988eaff3a02a920ae5530f8
Antgo99 <52596133+Antgo99@users.noreply.github.com>
2023-03-24 21:16:27 +0800

    Create 110.cpp

---
+++ binary_tree/reverse/110.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: Antgo99 616677561@qq.com
+ * @Date: 2023-03-20 19:48:56
+ * @LastEditors: Antgo99 616677561@qq.com
+ * @LastEditTime: 2023-03-20 20:24:43
+ * @FilePath: \LeetCode\binary_tree\reverse\110.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+};
+
+class Solution {
+public:
+    //刚开始没在深度判断阶段判断出左右节点高度超过1的情况 
+    int Nodedepth(TreeNode* root){
+        if(root == NULL) return 0;
+
+        int leftNode = Nodedepth(root->left);
+        if(leftNode == -1) return -1;
+        int rightNode = Nodedepth(root->right);
+        if(rightNode == -1) return -1;
+        return abs(leftNode-rightNode) > 1 ? -1 : 1+max(leftNode,rightNode);
+    }
+    bool isBalanced(TreeNode* root) {
+        return Nodedepth(root) == -1 ? false : true;
+    }
+};
